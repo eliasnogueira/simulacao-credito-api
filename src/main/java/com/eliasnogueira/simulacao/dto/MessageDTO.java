cff6307906d96b26ea346494f06756b80faffcfd
eliasnogueira <elias@backbase.com>
2020-02-01 17:34:41 +0100

    removed boilerplate code

--- src/main/java/com/eliasnogueira/simulacao/dto/MessageDTO.java
+++ src/main/java/com/eliasnogueira/simulacao/dto/MessageDTO.java
@@ -2,20 +2,14 @@ package com.eliasnogueira.simulacao.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 @ApiModel(value = "Mensagem")
 public class MessageDTO {
 
     @ApiModelProperty(required = true, example = "O CPF 999999999 não foi encontrado")
     private String mensagem;
-
-    public MessageDTO(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    public MessageDTO(){}
-
-    public String getMensagem() {
-        return mensagem;
-    }
 }
--- src/main/java/com/eliasnogueira/simulacao/dto/RestricaoDTO.java
+++ src/main/java/com/eliasnogueira/simulacao/dto/RestricaoDTO.java
@@ -2,18 +2,12 @@ package com.eliasnogueira.simulacao.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+@Data
 @ApiModel(value = "Restrição v1")
 public class RestricaoDTO {
 
     @ApiModelProperty(required = true, example = "99999999999")
     private String cpf;
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 }
