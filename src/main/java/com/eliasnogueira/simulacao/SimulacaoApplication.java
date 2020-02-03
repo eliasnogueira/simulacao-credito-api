238a4b2e41cc937ab5a6a984503a28cc05945383
eliasnogueira <elias.nogueira@gmail.com>
2020-02-03 11:37:50 +0100

    adicionado log apos inicalizacao

--- src/main/java/com/eliasnogueira/simulacao/SimulacaoApplication.java
+++ src/main/java/com/eliasnogueira/simulacao/SimulacaoApplication.java
@@ -24,15 +24,18 @@
 
 package com.eliasnogueira.simulacao;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@Log4j2
 public class SimulacaoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimulacaoApplication.class, args);
+        log.info("Aplicação iniciada! Ótimos testes!");
     }
 }
