3854723e6cd9b1d18bc942175fb344bcce23f7de
eliasnogueira <elias.nogueira@gmail.com>
2020-06-30 19:04:15 +0200

    swagger update

--- src/main/java/com/eliasnogueira/simulacao/swagger/SwaggerConfig.java
+++ src/main/java/com/eliasnogueira/simulacao/swagger/SwaggerConfig.java
@@ -24,9 +24,9 @@
 
 package com.eliasnogueira.simulacao.swagger;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.concurrent.ListenableFuture;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -34,10 +34,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     @Bean
