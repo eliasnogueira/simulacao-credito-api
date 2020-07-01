5884290a5c9495cb70064a773c6b62e528ec6738
eliasnogueira <elias.nogueira@gmail.com>
2020-07-01 18:07:58 +0200

    servico de consulta restricoes

---
+++ src/main/java/com/eliasnogueira/simulacao/service/RestricaoService.java
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) today.year Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.eliasnogueira.simulacao.service;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestricaoService {
+
+    @Value("${service.restricao.baseurl}")
+    private String baseUrl;
+
+    @Value("${service.restricao.port}")
+    private String port;
+
+    @Value("${service.restricao.endpoint}")
+    private String endpoint;
+
+    public static final String CPF = "{cpf}";
+
+    private final RestTemplate restTemplate;
+
+    public RestricaoService(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.setConnectTimeout(Duration.ofSeconds(5))
+            .setReadTimeout(Duration.ofSeconds(5)).build();
+    }
+
+    public boolean possuiRestricao(String cpf) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        final String uri = MessageFormat.format("{0}:{1}{2}/{3} ", baseUrl, port, endpoint, CPF);
+        return restTemplate.getForEntity(uri, Void.class, cpf)
+            .getStatusCode() == HttpStatus.OK;
+    }
+}
